5fbc4c39af6b0442fd4dbd095126bb7b74a098a2
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-18 12:21:47 +0800

    change http to https

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,63 +4,68 @@ import './App.css';
 
 class App extends React.Component {
   constructor(props) {
-      super(props)
-          this.state = {
-              baseUrl: 'http://taco-randomizer.herokuapp.com/',
-              url: 'http://taco-randomizer.herokuapp.com/random/?full-tack=true',
-              recipe_slug: '',
-              recipe_type: '',
-              searchUrl: '',
-              specificTaco: ''
-          }
+    super(props)
+    this.state = {
+      baseUrl: 'https://taco-randomizer.herokuapp.com/',
+      url: 'https://taco-randomizer.herokuapp.com/random/?full-tack=true',
+      recipe_slug: '',
+      recipe_type: '',
+      searchUrl: '',
+      specificTaco: ''
+    }
   }
   // call API
   fetchData = async () => {
+    try {
       const response = await fetch(this.state.url);
       const data = await response.json();
       this.setState({
-          data: data
+        data: data
       })
+    } catch (err) {
+      console.log(err);
+    }
+
   }
 
   // handle Change
   handleChange = (event) => {
-      this.setState({ [event.target.id]: event.target.value })
+    this.setState({ [event.target.id]: event.target.value })
   }
 
   // search specific Taco
   searchTaco = (event) => {
-      event.preventDefault()
-      const id = event.target.id;
-      const arr = id.split('');
-      arr.pop();
-      this.setState({
-          recipe_type: arr.join(''),
-          searchUrl: this.state.baseUrl + id + '/' + this.state.recipe_slug + '/'
-      }, async () => {
-          try {
-              const response = await fetch(this.state.searchUrl);
-              const data = await response.json();
-              this.setState({
-                  data: null,
-                  specificTaco: data
-              })
+    event.preventDefault()
+    const id = event.target.id;
+    const arr = id.split('');
+    arr.pop();
+    this.setState({
+      recipe_type: arr.join(''),
+      searchUrl: this.state.baseUrl + id + '/' + this.state.recipe_slug + '/'
+    }, async () => {
+      try {
+        const response = await fetch(this.state.searchUrl);
+        const data = await response.json();
+        this.setState({
+          data: null,
+          specificTaco: data
+        })
 
-          } catch (err) {
-              console.log(err);
-          }
-      })
+      } catch (err) {
+        console.log(err);
+      }
+    })
   }
 
   // fetch data right page load
   componentDidMount() {
-      this.fetchData()
+    this.fetchData()
   }
 
   render() {
-      return (
-          <Content fetchData={this.fetchData} data={this.state.data} searchTaco={this.searchTaco} handleChange={this.handleChange} recipe_type={this.state.recipe_type} specificTaco={this.state.specificTaco} />
-      )
+    return (
+      <Content fetchData={this.fetchData} data={this.state.data} searchTaco={this.searchTaco} handleChange={this.handleChange} recipe_type={this.state.recipe_type} specificTaco={this.state.specificTaco} />
+    )
   }
 }
 
