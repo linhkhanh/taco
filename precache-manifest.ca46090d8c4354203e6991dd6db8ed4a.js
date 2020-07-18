e9fe52cd49cd8672a31252d5d1b56f6908825d66
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-18 11:23:02 +0800

    Updates

---
+++ precache-manifest.ca46090d8c4354203e6991dd6db8ed4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83aa48292cb5ed1f4a93fff51ca04e3",
+    "url": "/taco/index.html"
+  },
+  {
+    "revision": "ac87bb62279285b501ab",
+    "url": "/taco/static/css/main.e4789a50.chunk.css"
+  },
+  {
+    "revision": "96322a585a2ab324640f",
+    "url": "/taco/static/js/2.e668036c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taco/static/js/2.e668036c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac87bb62279285b501ab",
+    "url": "/taco/static/js/main.9ff9eb0a.chunk.js"
+  },
+  {
+    "revision": "8d8d139c6de8b7ec2285",
+    "url": "/taco/static/js/runtime-main.c56548a3.js"
+  }
+]);
